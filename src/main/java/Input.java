17e98fe23d3b8f62e9cfe8834ef5913a0d8116ea
Vineeth <vineeth@autonomic.ai>
2026-02-09 12:13:10 -0500

    Read inputs

---
+++ src/main/java/Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public final class Input {
+
+  public String readInput() {
+    Scanner inputReader = new Scanner(System.in);
+    return inputReader.nextLine();
+  }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-         System.out.print("$ ");
-    }
+
+  public static void main(String[] args) throws Exception {
+    System.out.print("$ ");
+    Scanner inputReader = new Scanner(System.in);
+    String command = inputReader.nextLine();
+
+    System.out.println(command+": "+" command not found");
+  }
 }
